10f4a5d4fbe39f3d5927199cbcc98348fff065ac
Leonardo <l251773@dac.unicamp.br>
2025-02-18 09:41:15 -0300

    Adiciona método subtração

--- src/com/agibank/s5/git/Calculadora.java
+++ src/com/agibank/s5/git/Calculadora.java
@@ -5,7 +5,11 @@ public class Calculadora {
         return a+b;
     }
 
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) {
-        System.out.println("Resultado: " + soma(5,3));
+        System.out.println("Resultado da soma: " + soma(5,3));
+        System.out.println("Resultado da subtração: " + subtracao(30, 20));
     }
 }
