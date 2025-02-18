6f1c3007b3158c40a7dfa12684649159d0dfb290
Leonardo <l251773@dac.unicamp.br>
2025-02-18 09:03:31 -0300

    Adiciona classe calculadora

---
+++ src/com/agibank/s5/git/Calculadora.java
@@ -0,0 +1,11 @@
+package com.agibank.s5.git;
+
+public class Calculadora {
+    public static int soma(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Resultado: " + soma(5,3));
+    }
+}
